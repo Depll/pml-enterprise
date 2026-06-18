64fdde8e1fe4ee0217a20d7eb0e2fafe50d9c806
Depll <eevu2277@ukr.net>
2026-06-18 15:08:52 +0200

    chore: remove unused zutaten files from root directory

--- src/zutaten/zutaten.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('zutaten')
-export class ZutatenController {}
--- src/zutaten/zutaten.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ZutatenController } from './zutaten.controller';
-import { ZutatenService } from './zutaten.service';
-
-@Module({
-  controllers: [ZutatenController],
-  providers: [ZutatenService]
-})
-export class ZutatenModule {}
